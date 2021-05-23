27c582812baf289c607592d8dfec90cc9dd9a3a7
Newton Lomar <newtonlomar16@gmail.com>
2021-05-23 19:45:48 -0300

    imagens

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const port = 3000;
 
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public', 'index.html'));
 });
 
-
-
+app.post('/add', (req, res) => {
+    console.log(req.body);
+    res.send("feito");
+});
 
 app.listen(port, () => {
     console.log(`Servidor ligado na porta: ${port}`);
