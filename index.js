333e0ff8b5ff92c77b7cb7f012ffe9f0e0900a8b
Newton Lomar <newtonlomar16@gmail.com>
2021-05-24 08:56:19 -0300

    first steps querying

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const pool = require('./db');
 const port = 3000;
 
 
@@ -13,9 +14,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public', 'index.html'));
 });
 
-app.post('/add', (req, res) => {
-    console.log(req.body);
-    res.send("feito");
+app.post('/add', async (req, res) => {
+    let mudar1;
+    let mudar2;
+    try {
+        const novoProduto = await pool.query(`INSERT INTO produtos_base (nome, quantidade) VALUES (${mudar1} ${mudar2})`);
+    } catch(error) {
+        console.error(error);
+    }
 });
 
 app.listen(port, () => {
