7937d12639e192c38cfd60a4edd09746933c6358
Newton Lomar <newtonlomar16@gmail.com>
2021-05-25 09:16:40 -0300

    me entendendo com as querys

--- index.js
+++ index.js
@@ -16,9 +16,21 @@ app.get('/', (req, res) => {
 
 app.post('/add', async (req, res) => {
     try {
-        console.log(req.body);
+
+        const novoProduto = await pool.query(
+            "INSERT INTO produtos_base (nome, quantidade) VALUES ($1, $2) RETURNING *",
+            ['Exemplo', 1]
+        );
+
+        // for(produto in req.body) {
+
+        //     // let novoProduto = await pool.query(`INSERT INTO produtos_base (nome, quantidade) VALUES (${produto}, ${ Number(req.body[produto])})`);
+            
+        // }
+        res.json(novoProduto);
     } catch(error) {
         console.error(error);
+        res.send(error);
     }
 });
 
