ba9084f432da16a3fbdd460501168a644d9685e8
Newton Lomar <newtonlomar16@gmail.com>
2021-05-30 19:15:57 -0300

    finalmente conseguindo agrupar os itens por relatorio

--- public/psiu_comprador.js
+++ public/psiu_comprador.js
@@ -1,12 +1,25 @@
-const fetchProdutos = 
-    fetch('http://localhost:3000/teste')
-        .then(response => {
-            return response.json();
-        })
-        .then(produtos => {
-            console.log(produtos);
-        });
-
-for(let produto of fetchProdutos) {
-
-}
+const datasSetadas = [];
+const fetchProdutos = fetch('http://localhost:3000/teste')
+.then(response => {
+    return response.json();
+})
+.then(produtos => {
+    const datasRelatorios = [];
+    const relatorios = {};
+    for(let produto of produtos) {
+        datasRelatorios.push(produto.datarelatorio);
+    }
+    let setDatas = new Set(datasRelatorios);
+    for(let data of setDatas) {
+        datasSetadas.push(data);
+        relatorios[data] = [];
+    }
+    for(let produto of produtos) {
+        for(data in relatorios) {
+            if(produto.datarelatorio === data) {
+                relatorios[data].push(produto);
+            }
+        }
+    }
+    console.log(relatorios);
+});
